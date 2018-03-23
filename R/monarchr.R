60ce173d9ef6d919a93a0a6ca7c5547d67b245a6
chuck carey <charlieccarey@gmail.com>
2018-03-23 01:36:31 -0700

    iplemented bioentity_gene_homologs

--- R/monarchr.R
+++ R/monarchr.R
@@ -117,10 +117,123 @@ build_monarch_url <- function(path, query=NULL) {
   url
 }
 
+#' Flattens and concatenates character vectors in a list
+#'
+#' @param strings A list of character vectors.
+#'
+#' @return Character vector, same length as the initial list.
+#' @export
+#'
+#' @examples
+#' # words <- list("a", c("few", "words"), NA, NULL, c("to", "try")) # not passing...
+#' words <- list("a", c("few", "words"), NA, c("to", "try"))
+#' flatten_list_of_strings(words)
+flatten_list_of_strings <- function(strings) {
+  # TODO: protect against NULLs (warn, or convert to NAs?)
+  strings <- lapply(strings,
+                    function(x) {
+                      ifelse(length(x) > 1, paste(x, collapse = ', '), x)
+                    })
+  unlist(strings)
+}
 
-#' Gets homologs to a gene.
+#' Extracts and concatenates (possibly multiple) basenames from paths.
+#'
+#' Basenames are concatenated if there are multiple paths per input.
+#'
+#' @param paths A list of character vectors representing file or url paths.
+#'
+#' @return The basenames as a flattened character vector.
+#' @export
+#'
+#' @examples
+#' paths <- list('a/b.w', c('c/d.x', 'e/f.y'), 'g/h.z')
+#' list_of_paths_to_basenames(paths)
+list_of_paths_to_basenames <- function(paths) {
+  paths <- lapply(paths,
+                  function(x) {
+                    y <- sub("\\.[[:alnum:]]+$", '', basename(x))
+                  })
+  flatten_list_of_strings(paths)
+}
+
+#' Finds phrases from list of things, like a list of dataframes.
+#'
+#' Wherever the phrase exists, it is extracted. Convenient for list of
+#' dataframes in which we know the phrase occurs somewhere but we don't
+#' no the column, or don't care about preserving the source column name.
+#'
+#' @param things Lists. Maybe lists of dataframes.
+#' @param phrase An anchor phrase. (maybe to fetch cell matching that from df).
+#'
+#' @return List matching the phrase
+#' @export
+#'
+#' @examples
+#' #evidence <- extract_matching_phrases_from_lists(homs$evidence_graph.nodes, 'evidence')
+extract_matching_phrases_from_lists <- function(things, phrase) {
+  # TODO: protect and or warn against NULL in lists?
+  matches <- lapply(things,
+                    function(x) {
+                      y <- unlist(x)
+                      y[grepl(phrase, y)]
+                    })
+  matches <- flatten_list_of_strings(matches)
+  matches <- lapply(matches,
+                    function(x) ifelse(length(x) == 0, NA, x))
+  unlist(matches)
+}
+
+#' Gets homolog associations for a gene.
 #'
 #' The monarch_api class is included in return mainly for debugging.
+#'
+#' https://api.monarchinitiative.org/api/bioentity/gene/NCBIGene%3A8314?rows=100&fetch_objects=true
+#'
+#' @param gene A valid monarch initiative gene id.
+#'
+#' @return A list of (tibble of homolog information, monarch_api S3 class).
+#' @export
+#'
+#' @examples
+#' gene <-"NCBIGene:8314"
+#' bioentity_gene_homology_associations(gene)
+bioentity_gene_homology_associations <- function(gene) { # TODO: definitely want to add response taxons.
+  # TODO: Needs extensive testing with various NCBIGene and other IDs.
+  # TODO While going to all this trouble, probably should collect all associations at once into an S3 class.
+  # TODO: NCBIGene:8315 returns non-empty homology_associations, disease_associations, phenotype_associations,
+  # TODO: and several NULL other associations
+  # TODO: if lucky, we'll be able to process each in same manner?
+  gene <- utils::URLencode(gene, reserved = TRUE)
+
+  query <- list(rows=100, fetch_objects="true")
+  url <- build_monarch_url(path = list("/api/bioentity/gene", gene), # TODO: confirm this!!!
+                           query = query)
+  resp <- monarch_api(url)
+  homs <- jsonlite::flatten(resp$content$homology_associations, recursive=TRUE)
+
+  # dataframes embedded in lists are most problematic as they aren't flattened.
+
+  evids <- extract_matching_phrases_from_lists(homs$evidence_graph.nodes, 'evidence')
+  pubs <- extract_matching_phrases_from_lists(homs$publications, 'PMID')
+  sources <- list_of_paths_to_basenames(homs$provided_by)
+  homs <- homs[!names(homs) %in% c("publications", "provided_by")]
+  homs <- homs[c("subject.taxon.label", "subject.id", "subject.label", "relation.label",  "object.label", "object.id", "object.taxon.label")]
+  homs <- do.call('cbind.data.frame',
+                  list(homs,
+                       evidence = evids,
+                       publications = pubs, provided_by = sources))
+  return(list(homologs = homs, response = resp))
+}
+
+#' Get homologs for a gene.
+#'
+#' Replicates info in view: https://api.monarchinitiative.org/api/#!/bioentity/get_gene_homolog_associations
+#'
+#' The monarch_api class is included in return mainly for debugging.
+#'
+#' https://api.monarchinitiative.org/api/bioentity/gene/NCBIGene%3A8314/homologs/?rows=100&fetch_objects=true
+#'
 #'
 #' @param gene A valid monarch initiative gene id.
 #'
@@ -131,10 +244,32 @@ build_monarch_url <- function(path, query=NULL) {
 #' gene <-"NCBIGene:8314"
 #' bioentity_homologs(gene)
 bioentity_homologs <- function(gene) { # TODO: definitely want to add response taxons.
+  # TODO: Needs extensive testing with various NCBIGene and other IDs.
   gene <- utils::URLencode(gene, reserved = TRUE)
+
   query <- list(rows=100, fetch_objects="true")
-  url <- build_monarch_url(path = list("/api/bioentity/gene", gene, "homologs/"), query = query)
+  url <- build_monarch_url(path = list("/api/bioentity/gene", gene, "homologs/"),
+                           query = query)
   resp <- monarch_api(url)
-  homologs <- tibble::tibble()
-  return(list(homologs = homologs, response = resp))
+  homs <- jsonlite::flatten(resp$content$associations, recursive=TRUE)
+
+  #   debugging:
+  #     homs <- as_tibble(homs[, !apply(is.na(homs), 2, all)]) # deselect columns w/ no info
+  #     str(homs, max.level = 1) # OR
+  #     as_tibble(homs).
+
+  evids <- extract_matching_phrases_from_lists(homs$evidence_graph.nodes, 'evidence')
+  # TODO: We need to know all publications types so we don't miss any. e.g. for NCBIGene:8314, we miss the only publications "ZFIN:ZDB-PUB-030905-1"
+  # TODO: OR this is a case where we really do want to drill into data.frames within the lists?
+  pubs <- extract_matching_phrases_from_lists(homs$publications, 'PMID')
+  sources <- list_of_paths_to_basenames(homs$provided_by)
+  homs <- homs[!names(homs) %in% c("publications", "provided_by")]
+  homs <- homs[c("subject.taxon.label", "subject.id", "subject.label", "relation.label",  "object.label", "object.id", "object.taxon.label")]
+  homs <- do.call('cbind.data.frame',
+                  list(homs,
+                       evidence = evids,
+                       publications = pubs, provided_by = sources))
+  return(list(homologs = homs, response = resp))
 }
+
+
